4c216e74438be8a34e03cbca7e64c31a81e449a8
Basay <Basay1980@gmail.com>
2019-07-02 14:43:06 +0300

    исправил замечания: переименовал функции, сделал функцию рендом в одну строку, учел размеры блока с иконкой при создании массива с обьектами

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
 function randomInt(min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
+  return Math.floor(min + Math.random() * (max + 1 - min)); // собрал в одну строку
 }
 var avatarNumber = 1; // нельзя повторять аватары, для этого создал эту переменную, потом ее наращиваю
 var types = ['palace', 'flat', 'house', 'bungalo'];
-var setObject = function () { // создаем функцию для создания обьекта
+var createObject = function () { // создаем функцию для создания обьекта
   var object = {
     'author': {
       'avatar': 'img/avatars/user0' + avatarNumber++ + '.png',
@@ -16,24 +14,24 @@ var setObject = function () { // создаем функцию для созда
       'type': types[randomInt(0, 3)],
     },
     'location': {
-      'x': randomInt(0, 100),
+      'x': randomInt(25, (document.querySelector('.map__pins').offsetWidth - 25)),
       'y': randomInt(130, 630)
     }
   };
   return object;
 };
-var makeArrayObj = function (x) { // функция создает массив обьектов, количество элементов их икса
+var makeArrayObjects = function (x) { // функция создает массив обьектов, количество элементов их икс
   var array = [];
   for (var i = 0; i < x; i++) {
-    array[i] = setObject();
+    array[i] = createObject();
   }
   return array;
 };
-var arrayObj = makeArrayObj(8);
+var arrayObj = makeArrayObjects(8);
 var makeMapFragment = function (template) { // зачем то сделал функцию для создания фрагмента
   for (var i = 0; i < arrayObj.length; i++) {
     var element = template.cloneNode(true);
-    element.style = 'left:' + arrayObj[i].location.x + '%; top: ' + arrayObj[i].location.y + 'px;';
+    element.style = 'left:' + arrayObj[i].location.x + 'px;  top: ' + arrayObj[i].location.y + 'px;';
     element.querySelector('img').src = arrayObj[i].author.avatar;
     fragment.appendChild(element);
   }
