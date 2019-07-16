0910f09e24ba5ac81c41edec7eef04e522022a87
Basay <Basay1980@gmail.com>
2019-07-16 22:23:00 +0300

    сделал перемещение указателяб не сделал ограничение, не сделал расчет острого конца,

--- js/main.js
+++ js/main.js
@@ -58,18 +58,39 @@ var removeFogInStartWindow = function () {
 var elementsList = document.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinElement = document.querySelector('.map__pin--main');
 
 
 disableInputsAndSelects('.ad-form');
 disableInputsAndSelects('.map__filters');
-document.querySelector('.map__pin--main').addEventListener('click', function () {
+pinElement.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
   elementsList.appendChild(makeMapFragment(pinTemplate));
   removeFogInStartWindow();
   enableInputsAndSelects('.ad-form');
   enableInputsAndSelects('.map__filters');
-  document.querySelector('.map__pins').addEventListener('mouseup', function (evt) {
-    document.querySelector('#address').value = evt.clientX + ', ' + evt.clientY;
-  });
+  var startCoord = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  var onMouseMove = function (moveEvt) {
+    var shift = {
+      x: startCoord.x - moveEvt.clientX,
+      y: startCoord.y - moveEvt.clientY
+    };
+    startCoord = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    pinElement.style.top = (pinElement.offsetTop - shift.y) + 'px';
+    pinElement.style.left = (pinElement.offsetLeft - shift.x) + 'px';
+  };
+  var onMouseUp = function (upEvt) {
+    document.querySelector('#address').value = upEvt.clientX + ', ' + upEvt.clientY;
+    elementsList.removeEventListener('mousemove', onMouseMove);
+  };
+  elementsList.addEventListener('mousemove', onMouseMove);
+  elementsList.addEventListener('mouseup', onMouseUp);
 });
 
 
